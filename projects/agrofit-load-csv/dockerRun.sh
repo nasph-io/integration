58e313a20b0447518afec55d8037979defc6d862
Edgar Silva <edgar@skalena.com>
2023-09-06 20:36:25 -0300

    first commits

--- projects/agrofit-load-csv/dockerRun.sh
+++
@@ -1,4 +0,0 @@
-echo 'Executando Scrapper Service'
-docker run --name scrapper --mount source=dataset,target=/app/dataset registry.gitlab.com/embrapa-api/agrofit/node-scrapping
-echo 'Removendo Scrapper Service'
-docker container rm scrapper
